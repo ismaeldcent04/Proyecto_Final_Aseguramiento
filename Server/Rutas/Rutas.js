facec8db393d0cf718772fe8f6b876833d0ecad3
ismaeldcent04 <87149086+ismaeldcent04@users.noreply.github.com>
2022-10-13 22:17:28 -0400

    klsda

--- Server/Rutas/Rutas.js
+++ Server/Rutas/Rutas.js
@@ -19,23 +19,42 @@ router.get("/register", (req, res) => {
 router.post("/register", (req, res) => {
   console.log(req.body.email);
   res.send(req.body.nombre);
-  // const newUser = new User({
-  //   nombre: req.body.nombre,
-  //   email: req.body.email,
-  //   contraseña: req.body.contraseña,
-  //   idusuario: req.body.idusuario,
-  // });
-  // newUser.save(function (err) {
-  //   if (!err) {
-  //     res.send("Usuario agregado correctamente");
-  //   } else {
-  //     res.send(err);
-  //   }
-  // });
+  const newUser = new User({
+    nombre: req.body.nombre,
+    email: req.body.email,
+    contraseña: req.body.contraseña,
+    idusuario: req.body.idusuario,
+  });
+  newUser.save(function (err) {
+    if (!err) {
+      res.send("Usuario agregado correctamente");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
 router.get("/login", (req, res) => {
   res.end("Hola esta es la pagina de login");
+});
+router.post("/login", (req, res) => {
+  const nombre = req.body.nombre;
+  const contraseña = req.body.contraseña;
+  console.log(nombre, contraseña);
+
+  User.findOne({ nombre: nombre }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.contraseña === contraseña) {
+          res.send("Usuario existe, entrando al home....");
+        } else {
+          res.send("Credenciales incorrectas, por favor intenta de nuevo");
+        }
+      }
+    }
+  });
 });
 
 router.get("/home", (req, res) => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Components/Pages/Auth/Login'
-import SignUp from '/Components/Pages/Auth/SignUp'
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Pages/Auth/Login";
+import SignUp from "./Components/Pages/Auth/SignUp";
 
 function App() {
   return (
     <Router>
-        <div className="app">
+      <div className="app">
         <div className="App-header">
           <div className="auth-inner">
             <Routes>
-              <Route exact path="/" element={<Login/>} />
-              <Route path="/sign-up" element={<SignUp/>} />
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
             </Routes>
           </div>
         </div>
-        </div>
+      </div>
     </Router>
-  )
+  );
 }
-export default App
-
+export default App;
