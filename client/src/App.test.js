c8257fe9fb20a0383275e10ce58488101d672dc7
Joel Abreu <89609669+joel-codemera@users.noreply.github.com>
2022-10-14 21:14:39 -0400

    Adding unit tests to run in CI

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/*SIGN UP SECTION */
+router.get("/sign-up", (req, res) => {
+  res.end("Hola esta es la pagina de registro");
+});
+
+router.post("/sign-up", (req, res) => {
+  console.log(req.body.email);
+
+  const newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (!err) {
+      console.log("Succesfully added new user");
+    } else {
+      res.end(err);
+    }
+  });
 });
